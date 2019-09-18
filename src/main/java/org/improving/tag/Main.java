11f1ec1631158bcdd270de9d4e13dd0b4c2c4321
Ethan Miller <Ethan.Miller@improving.com>
2019-09-18 15:36:38 -0500

    add game and person

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -4,25 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean loop = true;
-        while (loop) {
-            System.out.print("> ");
-            String input = scanner.nextLine().trim();
-            if (input.equals("look")) {
-                System.out.println("You look around.");
-            } else if (input.equals("inventory")) {
-                System.out.println("You are carrying nothing.");
-            } else if (input.equals("dance")) {
-                System.out.println("You dance around.");
-            } else if (input.equals("jump")) {
-                System.out.println("You jump in the air.");
-            } else if (input.equals("exit")) {
-                System.out.println("Goodbye.");
-                loop = false;
-            } else {
-                System.out.println("Huh? I don't understand.");
-            }
-        }
+        Game game = new Game();
+        game.run();
+
+        long elapsedTicks = game.getEndTime().getTime() - game.getStartTime().getTime();
+        double elapsedSeconds = elapsedTicks / 1000.0;
+        System.out.println("We were running for " + elapsedSeconds + "s.");
     }
 }
---
+++ src/main/java/org/improving/tag/Person.java
@@ -0,0 +1,71 @@
+package org.improving.tag;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Person {
+    private String name;
+    private String dateOfBirth;
+    private String address;
+    private String city;
+    private String state;
+    private Integer zip;
+
+    public Person() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        this.name = scanner.nextLine();
+        System.out.print("Please enter your date of birth: ");
+        this.dateOfBirth = scanner.nextLine();
+        System.out.print("Please enter your city: ");
+        this.city = scanner.nextLine();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(Integer zip) {
+        this.zip = zip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public Integer getZip() {
+        return zip;
+    }
+}
