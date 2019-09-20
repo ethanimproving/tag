071b2cb1cc1f80b165d623fd9ab6ebac559597e8
Ethan Miller <Ethan.Miller@improving.com>
2019-09-20 09:57:59 -0500

    implement spring context

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -1,14 +1,19 @@
 package org.improving.tag;
 
+import org.improving.tag.commands.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start of main");
-        Game game = new Game();
-        System.out.println("Declared game");
+
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringContext.class);
+
+        Game game = context.getBean(Game.class);
         game.run();
-        System.out.println("After run");
 
         long elapsedTicks = game.getEndTime().getTime() - game.getStartTime().getTime();
         double elapsedSeconds = elapsedTicks / 1000.0;
